66ca2167696bf8b3f07311dd27c1d41eb639f458
Peter Smith <peteylynnsmith@gmail.com>
2016-07-20 12:14:05 -0700

    ask tyler for help

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,10 +1,3 @@
-# I couldn't figure out how to make either of the loops required.
-# I tried the employees processed one and it didn't work but 
-# I couldn't come up with anything close for the loop asking 
-# allergies. I would have continued trying but I am out of time.
-# I will be in office hours to discuss what I don't know about
-#implementing loops.
-
 
 #puts "How many employees will be processed?"
 #	employees_processed = gets.chomp
@@ -14,56 +7,68 @@
 #	number = number + 1
 #	end
 
-#def vamp_detector
-puts "What's your name?"
-	name = gets.chomp
-	name.downcase!
-if name == "drake cula"
-	abort ("Definitely a vampire")
-elsif name == "tu fang"
-	abort ("Definitely a vampire")
-end
-puts "How old will you be this year?"
-	age = gets.chomp.to_i
-puts "What year were you born?"
-	year_of_birth = gets.chomp.to_i
-puts "We like eating garlic bread, do you eat garlic bread?"
-	garlic_answer = gets.chomp
-puts "Do you require health insurance?"
-	mortal = gets.chomp
+#def vampire_detector
 
-	if year_of_birth + age == 2016
+#def vampire_detector(number_of_employees)
+	puts "What's your name?"
+		normal_name = gets.chomp
+		normal_name.downcase!
+	puts "How old will you be this year?"
+		age = gets.chomp.to_i
+	puts "What year were you born?"
+		year_of_birth = gets.chomp.to_i
+	puts "We like eating garlic bread, do you eat garlic bread?"
+		garlic_answer = gets.chomp
+	puts "Do you require health insurance?"
+		mortal = gets.chomp
+	puts "List your allergies, entering them one at a time, followed by entering the phrase 'done'"
+		allergy = gets.chomp	
+	
+#	return allergy
+#end
+
+
+until allergy == "done" || allergy == "sunshine"
+end
+	
+
+if normal_name == "drake cula" || normal_name == "tu fang"
+	normal_name = false
+end
+
+if garlic_answer == "yes"
+	likes_garlic = true
+elsif garlic_answer == "no"
+	likes_garlic == false
+end 
+
+if mortal == "yes"
+	not_immortal = true
+elsif mortal == "no"
+	not_immortal = false
+end
+
+	if normal_name == false
+		puts "definitely a vampire"
+	elsif year_of_birth + age == 2016
 		knows_age = true
-	elsif
-		year_of_birth + age != 2016
-		knows_age = false
-	end
-	case garlic_answer
-	when "yes"
-		likes_garlic = true
+	elsif allergy == true || !knows_age && ( !likes_garlic || !not_immortal ) 
+		puts "probably a vampire"
+	elsif knows_age && likes_garlic && not_immortal
+		puts "probably not a vampire"
+	elsif !knows_age && !likes_garlic && !not_immortal
+		puts "almost certainly a vampire"
 	else
-		likes_garlic = false
+		puts "results inconclusive"
 	end
-	case mortal 
-	when "yes"
-		not_immortal = true
-	else
-		not_immortal = false
-	end
+	#puts "How many employees will we be processing? (1,2,3 etc.)"
+#	number_of_employees = gets.chomp
+#	employees == 0
+#	until employees == number_of_employees
+#		puts vampire_detector 
+#	employees += 1
+#	end
 
-if knows_age && likes_garlic && not_immortal
-	puts "probably not a vampire"
-elsif !knows_age && !likes_garlic && !not_immortal
-	puts "almost certainly a vampire"
-elsif knows_age == false
-	puts "probably a vampire"
-elsif likes_garlic == false
-	puts "probably a vampire"
-elsif not_immortal == false
-	puts "probably a vampire"
-else
-	puts "results inconclusive"
-end
 
 puts "Actually, never mind! What do these questions have to do with anything? Let's all be friends."
 #end
