b9958e255ef156df2d2d65df091440f2e6580fa1
Peter Smith <peteylynnsmith@gmail.com>
2016-07-23 10:34:18 -0700

    finish iteration

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -9,7 +9,7 @@ end
 Blurt_Nothing  { |number1, number2| puts "Yo, this is #{number1} and #{number2}, and I am the yield."}
 
 
-car_maker = ["Honda" , "Toyota" , "Ford"]
+car_maker = ["Honda" , "Toyota" , "Ford", "Tesla", "Saturn", "Nissan", "Lamborghini", "Chevrolet"]
 puts car_maker
 
 car_maker.each do |maker| 
@@ -18,13 +18,14 @@ car_maker.each do |maker|
 end
 
 car_maker.map! do |maker|
-    maker.upcase	#removed p 
+    maker.upcase
 end
 
-puts car_maker #changed p to puts
+puts car_maker
 
-				#this is a key(index) => value(as in array)
-car_make_model = { "Honda" => "Civic" , "Toyota" => "Prius" , "Ford" => "F-150"}
+
+car_make_model = { "Honda" => "Civic" , "Toyota" => "Prius" , "Ford" => "F-150", "Tesla" => "Model3", 
+"Saturn" => "Psi", "Lamborghini" => "Veneno Roadster", "Chevrolet" => "Impala" }
 puts car_make_model
 
 car_make_model.each do |maker, model| 
@@ -35,8 +36,26 @@ end
 car_maker.delete_if { |maker| maker.length <= 4}
 p car_maker
 
-car_maker.keep_if { |maker| maker.length > 4}
+car_maker.keep_if { |maker| maker.length >= 6}
 p car_maker
 
-car_maker.select! {|maker| maker == "Toyota"}
+car_maker.select! {|maker| maker.include? "A"}
 p car_maker
+
+car_maker.map!.take_while { |maker| maker.length == 6}
+p car_maker
+
+car_make_model.delete_if {|make, model| model.include? "C"}
+p car_make_model
+
+car_make_model.keep_if {|make, model| model.length > 4}
+p car_make_model
+
+car_make_model.select! {|make, model| model.length > 5}
+p car_make_model
+
+car_make_model = { "Honda" => "Civic" , "Toyota" => "Prius" , "Ford" => "F-150", "Tesla" => "Model3", 
+"Saturn" => "Psi", "Lamborghini" => "Veneno Roadster", "Chevrolet" => "Impala" }
+
+car_make_model.reject! {|make, model| model.include? "i" }
+p car_make_model
