fa20c1e62c1e9a5dc8cafdfdaf92189bb13f7f2c
Peter Smith <peteylynnsmith@gmail.com>
2016-07-23 13:39:07 -0700

    add hashes release

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -1,17 +1,54 @@
-puts "Whats your name?"
-	responses[name:] = gets.chomp
-puts "whats your address?"
-	address=gets.chomp
-puts "whats your email?"
-	email=gets.chomp
-puts "whats your phone number?"
-	phone=gets.chomp
-puts "whats your favorite shade of blue?"
-	blue=gets.chomp
-puts "would you prefer paisley, chevrons, photoreaistic woodsy scenes, or abstract woods scenes wallpaper?"
-	wallpaper=gets.chomp
-puts "is ombre fierce, so last season, or Practically medieval in its appalling irrelevance"
-	ombre=gets.chomp
+#gather info and input responses to hash
+responses = {}
+puts "Whats the client's name?"
+	responses[:name] = gets.chomp
+puts "whats your client's age?"
+	responses[:age] = gets.chomp
+puts "whats your client's email?"
+	responses[:email] = gets.chomp
+puts "whats your client's phone number?"
+	responses[:phone] = gets.chomp
+puts "whats your client's decor theme?"
+	responses[:theme] = gets.chomp
+puts "does your client like indoor plants?"
+	responses[:likes_plants] = gets.chomp
+p responses
+#offer a way to correct an input
+puts "if you need to change something, what is the key of what you need to change? (see hash), otherwise enter 'none'"
+	correction = gets.chomp
+#deal with their response
+if correction == "none"
+	puts "great job!"
 
-responses {their_name: name, address:, }
+elsif correction == ":name"
+	puts "what is your new value for it?"
+	responses[:name] = gets.chomp
+	p responses
+
+elsif correction == ":age"
+	puts "what is your new value for it?"
+	responses[:age] = gets.chomp
+	p responses
+
+elsif correction == ":email"
+	puts "what is your new value for it?"
+	responses[:email] = gets.chomp
+	p responses
+
+elsif correction == ":phone"
+	puts "what is your new value for it?"
+	responses[:phone] = gets.chomp
+	p responses
+
+elsif correction == ":theme"
+	puts "what is your new value for it?"
+	responses[:theme] = gets.chomp
+	p responses
+
+elsif correction == ":likes_plants"
+	puts "what is your new value for it?"
+	responses[:likes_plants] = gets.chomp
+	p responses
+end
+
 
