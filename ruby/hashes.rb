5c66e3782566eefcba0d7816313dbba57a5b7279
Peter Smith <peteylynnsmith@gmail.com>
2016-07-24 09:11:32 -0700

    hashes first draft

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -3,51 +3,64 @@ responses = {}
 puts "Whats the client's name?"
 	responses[:name] = gets.chomp
 puts "whats your client's age?"
-	responses[:age] = gets.chomp
+	responses[:age] = gets.chomp.to_i
 puts "whats your client's email?"
 	responses[:email] = gets.chomp
 puts "whats your client's phone number?"
-	responses[:phone] = gets.chomp
+	responses[:phone] = gets.chomp.to_i
 puts "whats your client's decor theme?"
 	responses[:theme] = gets.chomp
-puts "does your client like indoor plants?"
-	responses[:likes_plants] = gets.chomp
+puts "does your client like indoor plants? (y/n)"
+	plant_pref = gets.chomp
+	if plant_pref == "y" 
+		responses[:likes_plants] = true
+	elsif plant_pref == "n" 
+		responses[:likes_plants] = false
+	end
+
 p responses
 #offer a way to correct an input
 puts "if you need to change something, what is the key of what you need to change? (see hash), otherwise enter 'none'"
 	correction = gets.chomp
 #deal with their response
-if correction == "none"
-	puts "great job!"
 
-elsif correction == ":name"
+
+if correction == "none"
+	puts "Great job!"
+end
+if correction == ":name" || correction == "name"
 	puts "what is your new value for it?"
 	responses[:name] = gets.chomp
 	p responses
-
-elsif correction == ":age"
+end
+if correction == ":age" || correction == "age"
 	puts "what is your new value for it?"
-	responses[:age] = gets.chomp
+	responses[:age] = gets.chomp.to_i
 	p responses
-
-elsif correction == ":email"
+end
+if correction == ":email" || correction == "email"
 	puts "what is your new value for it?"
 	responses[:email] = gets.chomp
 	p responses
-
-elsif correction == ":phone"
+end
+if correction == ":phone" || correction == "phone"
 	puts "what is your new value for it?"
-	responses[:phone] = gets.chomp
+	responses[:phone] = gets.chomp.to_i
 	p responses
-
-elsif correction == ":theme"
+end
+if correction == ":theme" || correction == "theme"
 	puts "what is your new value for it?"
 	responses[:theme] = gets.chomp
 	p responses
-
-elsif correction == ":likes_plants"
+end
+if correction == ":likes_plants" || correction == "likes_plants" || correction == "likes plants"
 	puts "what is your new value for it?"
-	responses[:likes_plants] = gets.chomp
+	plant_pref = gets.chomp
+	if plant_pref == "y" 
+		responses[:likes_plants] = true 
+	elsif plant_pref == "n" 
+		responses[:likes_plants] = false
+	end
 	p responses
 end
 
