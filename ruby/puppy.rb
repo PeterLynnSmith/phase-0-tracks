e5b116242458efd210adc413eeae10ace0a98de8
Peter Smith <peteylynnsmith@gmail.com>
2016-09-07 21:57:31 -0700

    makes a puppy class

---
+++ ruby/puppy.rb
@@ -0,0 +1,16 @@
+#class PUPPIES!
+#characteristics color,  size, 
+
+#behavior loudness, violentness
+
+class Puppy
+end
+
+cody = Puppy.new
+daisy = Puppy.new
+
+cody.class
+#this will give me Puppy
+
+cody.class == daisy.class
+#this gives me true
