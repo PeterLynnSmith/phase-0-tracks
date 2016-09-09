3731bc45bb7521f31f9402cf94cded743ea65304
Peter Smith <peteylynnsmith@gmail.com>
2016-09-09 14:18:07 -0700

    finish until office hours

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -28,13 +28,48 @@ class Puppy
 end
 
 #driver code
-a = Puppy.new
-a.fetch("ball")
+# a = Puppy.new
+# a.fetch("ball")
 
-a.speak(4)
+# a.speak(4)
 
-a.roll_over
+# a.roll_over
 
-puts a.dog_years(3)
+# puts a.dog_years(3)
 
-a.human_talk
+# a.human_talk
+class Gymnast
+	def initialize
+		puts "initializing Gymnast class" 
+	end
+	def flip
+		puts "did a flip!"
+	end
+	def jump
+		puts "jumped super high!"
+	end
+end
+
+a = Gymnast.new
+
+puts a
+
+classes = []
+counter = 0
+until counter == 50
+	class_name = counter 
+	class_name = Gymnast.new
+	classes << class_name 
+	counter = counter + 1
+	
+end
+
+p classes
+
+classes.each do |instance|
+	puts "#{instance} #{instance.flip} d #{instance.jump}"
+end
+
+
+
+
