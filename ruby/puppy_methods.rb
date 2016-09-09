4c5ec57a0464fee4ce8d22f14813235fc64e5f16
Peter Smith <peteylynnsmith@gmail.com>
2016-09-09 13:23:47 -0700

    complete release 0

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,5 +5,32 @@ class Puppy
     toy
   end
 
+  def speak(times)
+  	puts "Woof" * times
+  end
+
+  def roll_over
+  	puts "rolls over"
+  end
+
+  def dog_years(years)
+  	years * 10.5
+  end
+
+  def human_talk
+  	puts "How do you do, human?"
+  end
+
 end
 
+#driver code
+a = Puppy.new
+a.fetch("ball")
+
+a.speak(4)
+
+a.roll_over
+
+puts a.dog_years(3)
+
+a.human_talk
