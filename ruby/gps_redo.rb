c73bbba508ee06d1bba970f857d041fd03faa90f
Peter Smith <peteylynnsmith@gmail.com>
2016-09-12 22:05:58 -0700

    finish pseudocode

---
+++ ruby/gps_redo.rb
@@ -0,0 +1,53 @@
+# Method to create a list
+# input: string of items separated by spaces (example: "carrots apples cereal pizza")
+# steps: 
+  # separate string by spaces into array
+  # define an empty hash
+  # iterate over array and set hash key to each array element
+  # set default quantity (value)
+  # print the list to the console [can you use one of your other methods here?]
+# output: [what data type goes here? hash]
+
+# Method to add an item to a list
+# input: item name and optional quantity
+# steps: 
+	#take arguments of method and assign them to a hash key and value
+# output: hash (last evaluated expression)
+
+# Method to remove an item from the list
+# input: item to be deleted
+# steps: 
+	#take in item(key) and delete it
+# output: return hash
+
+# Method to update the quantity of an item
+# input: item(key) and new quantity
+# steps:
+	#set item to new quantity(value)
+# output: return hash
+
+# Method to print a list and make it look pretty
+# input: hash 
+# steps: 
+	#iterate through hash and print the keys and values through interpolation in a sentence
+# output: a bunch of sentences
+grocery_list = {}
+def create_list(grocery_list, item_list)
+	item_list = item_list.split
+	item_list.each do |item|
+		grocery_list[item] = 1
+	end
+	puts grocery_list
+	
+end
+
+def add (grocery_list, item, quant=1)
+	my_list[item] = quant
+end
+
+my_list = create_list("apples bananas pizza")
+
+
+
+
+
