576ee7bd727de5d488d8dc6078c8b049dc062980
Peter Smith <peteylynnsmith@gmail.com>
2016-09-13 14:18:05 -0700

    setup game

---
+++ ruby/redo-game/redo_game.rb
@@ -0,0 +1,16 @@
+class Game
+
+	def initialize
+
+	end
+
+	def word_to_array
+		
+	end
+
+end
+
+puts "User1, give me a word that User2 will guess letter-by-letter."
+	word = gets.chomp
+
+word_to_array(word)
