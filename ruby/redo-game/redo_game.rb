9ffbf91675ba485387f1326e43c0b391a9f37745
Peter Smith <peteylynnsmith@gmail.com>
2016-09-13 14:42:26 -0700

    turned answer-word into array

--- ruby/redo-game/redo_game.rb
+++ ruby/redo-game/redo_game.rb
@@ -1,16 +1,22 @@
 class Game
-
+	attr_reader :answer_array
 	def initialize
-
+		@answer_array = []
 	end
 
-	def word_to_array
-		
+	def word_to_array(string)
+		@answer_array = string.split('')
+		p @answer_array
 	end
+
+	
 
 end
 
 puts "User1, give me a word that User2 will guess letter-by-letter."
 	word = gets.chomp
 
-word_to_array(word)
+first_game = Game.new
+
+first_game.word_to_array(word)
+
