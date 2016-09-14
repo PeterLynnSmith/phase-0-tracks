ecbcf395190a9e64ce351177fb4d66db62330a23
Peter Smith <peteylynnsmith@gmail.com>
2016-09-13 20:53:19 -0700

    converts array to underscore array

--- ruby/redo-game/redo_game.rb
+++ ruby/redo-game/redo_game.rb
@@ -2,6 +2,7 @@
 	#promt for word
 	#turn word into array
 	#make an array that is an underscore version of the array
+	
 	#set the number of guesses that the user is allowed
 	#promt for guess
 	#if word array contains the guess, give feedback that is the
@@ -11,21 +12,36 @@
 
 class Game
 	attr_reader :answer_array
+	#attr_reader :feedback_array
+
 	def initialize
 		@answer_array = []
+		@feedback_array = []
 	end
 
 	def word_to_array(string)
 		@answer_array = string.split('')
-		p @answer_array
+	 	@answer_array
 	end
 
+	def underscore_array(array)
+		array.map do |letter|
+		 	letter = "_"
+		end
+	end
+	@answer_array
+	@feedback_array
 end
 
-puts "User1, give me a word that User2 will guess letter-by-letter."
+ puts "User1, give me a word that User2 will guess letter-by-letter."
 	word = gets.chomp
 
 first_game = Game.new
 
 first_game.word_to_array(word)
+p first_game.underscore_array(first_game.answer_array)
+
+#puts "User2, guess a letter that is contained in User1's word."
+
+
 
--- ruby/redo-game/redo_game_spec.rb
+++ ruby/redo-game/redo_game_spec.rb
@@ -7,4 +7,8 @@ describe Game do
 		expect(game.word_to_array("hello")).to eq ["h", "e", "l", "l", "o"]
 	end
 
+	it "makes a feedback array from the secret word array" do
+		expect(game.underscore_array(["h", "e", "l", "l", "o"])).to eq ["_", "_", "_", "_", "_"]
+	end
+
 end
